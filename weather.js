d281984e651e94c1135e9c300d6dfdf75692bd2f
kateramckay <kateradmckay@gmail.com>
2023-01-30 13:01:31 -0500

    added some more js

--- weather.js
+++ weather.js
@@ -2,5 +2,13 @@ import axios from "axios";
 // https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m
 
 export function getWeather(lat, lon,timezone) {
-    axios.get("https://api.open-meteo.com/v1/forecast")
+    return axios.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m", {
+    params: {
+        latitude: lat,
+        longitude: lon,
+        timezone,
+
+    }
+     }
+    )
 }
