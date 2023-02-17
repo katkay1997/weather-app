9d34cb66cda7c787f88a5aa714fe305a4e60ba5c
kateramckay <kateradmckay@gmail.com>
2023-02-17 09:31:46 -0500

    js

--- weather.js
+++ weather.js
@@ -32,6 +32,7 @@ function parseCurrentWeather({current_weather, daily}) {
         temperature_2m_min: [minTemp],
         apparent_temperature_max: [maxTemp],
         temperature_2m_max:[maxTemp],
+        precipitation_sum: [precip],
 
     }
 
