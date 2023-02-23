86bb745c2255f2b5db3b641b5bb2c9bbd54a4330
kateramckay <kateradmckay@gmail.com>
2023-02-23 13:38:12 -0500

    js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ import { getWeather } from "./weather"
 
 getWeather(10,10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
     res => {
-        console.log(res.data)
+        console.log()
     }
 )
