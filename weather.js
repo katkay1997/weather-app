45d130f2cc5e0619c22f9f7d147e5815b57d776d
kateramckay <kateradmckay@gmail.com>
2023-03-04 19:55:21 -0500

    js

--- weather.js
+++ weather.js
@@ -63,7 +63,7 @@ function parseDailyWeather({ daily }) {
 function parseHourlyWeather({hourly, current_weather}) {
     return hourly.time.map((time, index)) => {
         return {
-            timestamp: time * 1000, 
+            timestamp: time * 1000,  //37:25
         }
     }
 };
